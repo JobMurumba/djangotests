264040369e6e70478f4f649358b7ae7179a5fba0
JobMurumba <murumbajob78@gmail.com>
2020-07-09 14:32:44 +0300

    modified requirements

--- djangotests/settings.py
+++ djangotests/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whatever',
-    'django_jenkins'
+    'django_jenkins',
+    'coverage'
 ]
 
 MIDDLEWARE = [
